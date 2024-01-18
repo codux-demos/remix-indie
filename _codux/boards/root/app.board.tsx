860d6dfb3c5858f69e66719ff3a00323952fe7d0
Sagiv Dayan <sagiv.keeper@gmail.com>
2024-01-18 14:59:20 +0200

    `isSnippet: false` in app.board.tsx

--- _codux/boards/root/app.board.tsx
+++ _codux/boards/root/app.board.tsx
@@ -1,9 +1,9 @@
 import { createBoard } from "@wixc3/react-board";
 
-import { PageWrapper } from '../../board-wrappers/page-wrapper';
+import { PageWrapper } from "../../board-wrappers/page-wrapper";
 
 export default createBoard({
-    name: "App",
-    Board: () => <PageWrapper path="/" />,
-    isSnippet: true,
+  name: "App",
+  Board: () => <PageWrapper path="/" />,
+  isSnippet: false,
 });
